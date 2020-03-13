625116d79f3fc3d4c7837301c594615dee091a75
GedMarc <ged_marc@hotmail.com>
2020-03-13 10:34:19 +0200

    #2 - Remove com.google.guice from module-info definitions

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -25,7 +25,5 @@ module com.guicedee.guicedservlets.jsf {
 	provides com.guicedee.guicedservlets.undertow.services.UndertowDeploymentConfigurator with com.guicedee.guicedservlets.jsf.implementations.GuicedJSFDeploymentInfoConfiguration;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceModule with com.guicedee.guicedservlets.jsf.implementations.JsfNamedBinder;
 
-	opens com.guicedee.guicedservlets.jsf to com.google.guice;
 	//opens com.guicedee.schemas.facesconfig to java.xml.bind;
-	opens com.guicedee.guicedservlets.jsf.implementations to com.google.guice;
 }
